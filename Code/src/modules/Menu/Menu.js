04949a091456a2695e581700db9808eba8006ac6
BenC0 <suddensquirrel94@gmail.com>
2019-05-04 15:20:19 +0100

    Hamburger click event implemented

--- Code/src/modules/Menu/Menu.js
+++ Code/src/modules/Menu/Menu.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './Menu.scss';
 import Themes from "../Themes/Themes"
 
+const b = document.body
+
 function Hamburger() {
 	return (
-		<div className="hamburger">
-				<span className="line tLine"></span>
-				<span className="line mLine"></span>
-				<span className="line bLine"></span>
+		<div className="hamburger" onClick={MenuClick}>
+			<span className="line tLine"></span>
+			<span className="line mLine"></span>
+			<span className="line bLine"></span>
 		</div>
 	)
+}
+
+export function MenuClick(event) {
+	let show = !b.classList.contains('showMenu')
+	let menu = event.currentTarget
+
+	if (show) {
+		menu.classList.add('active')
+		b.classList.remove('hideThemes')
+		b.classList.add('showMenu')
+	} else {
+		menu.classList.remove('active')
+		b.classList.add('hideThemes')
+		b.classList.remove('showMenu')
+	}
 }
 
 function Menu() {
