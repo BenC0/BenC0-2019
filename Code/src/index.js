64b8855f9a080ae21dda333ffd528c66f2b2b98d
BenC0 <suddensquirrel94@gmail.com>
2020-01-01 20:22:51 +0000

    Updated copy, implemented nav and improved misc styles

--- Code/src/index.js
+++ Code/src/index.js
@@ -5,11 +5,41 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './modules/App';
 import * as serviceWorker from './serviceWorker';
 
+function scroll_to(el) {
+    el = document.querySelector(el)
+    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+    let hH = document.querySelector('header').clientHeight
+    let tgt_pos = distanceToTop(el) - hH
+	window.scrollBy({ top: tgt_pos, left: 0, behavior: "smooth" });
+}
+
+function build_link(link) {
+	let name = link.getAttribute('id')
+	let section_name = link.querySelector('h2').textContent
+	return `<a link_name="${name}">${section_name}</a>`
+}
+
+function buildNav() {
+	let section = document.querySelectorAll('section')
+	let target = document.querySelector('nav')
+
+	section.forEach(section => {
+		let link = build_link(section)
+		target.insertAdjacentHTML('beforeend', link)
+		let link_selector = `[link_name="${section.getAttribute('id')}"]`
+		let link_el = target.querySelector(link_selector)
+		link_el.addEventListener('click', e => {
+			e.preventDefault()
+			scroll_to(`#${section.getAttribute('id')}`)
+			document.querySelector('.hamburger').click()
+		})
+	})
+}
+
 const routing = (
 	<Router>
 		<div>
 			<nav>
-				<Link to="/" className={(window.location.pathname === "/") ? "active" : ""}>Home</Link>
 			</nav>
 			<Route path="/" component={App} />
 		</div>
@@ -17,7 +47,7 @@ const routing = (
 )
 
 ReactDOM.render(routing, document.getElementById('root'))
-
+buildNav()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- Code/src/modules/Content/About.js
+++ Code/src/modules/Content/About.js
@@ -4,7 +4,7 @@ function About() {
 	return (
 		<section id="about">
 			<h2>Blurb</h2>
-			<p>I'm a Conversion Rate Optimisation specialist currently employed by iProspect Manchester. I utilise my expertise in web development, UI/UX design and behavioural psychology to optimise websites from a wide variety of sectors to improve their performance.</p>
+			<p>I'm a Conversion Rate Optimisation specialist currently employed by N Brown. I utilise my expertise in web development, UI/UX design and behavioural psychology to optimise websites from a wide variety of sectors to improve their performance.</p>
 		</section>
 	);
 }
